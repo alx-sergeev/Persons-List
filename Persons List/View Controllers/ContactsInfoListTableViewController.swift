56d9b3bf67234190c78c470761dc59348050a18b
Alexander Sergeev <sergeev_ul73@mail.ru>
2021-12-02 21:09:51 +0400

    Project: fix подготовительных работ вкладки Contacts Info

--- Persons List/View Controllers/ContactsInfoListTableViewController.swift
+++ Persons List/View Controllers/ContactsInfoListTableViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ContactsInfoListTableViewController: UITableViewController {
     
-    // MARK: - Outlets
-    @IBOutlet weak var contactsTableView: UITableView!
+    // MARK: - Properties
+    let cellName = "contactInfoCell"
+    let arContacts = Person.getContactsList()
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+}
+
+extension ContactsInfoListTableViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arContacts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellName, for: indexPath)
+        var contentCell = cell.defaultContentConfiguration()
+        
+        contentCell.text = "My test"
+        
+        cell.contentConfiguration = contentCell
+        
+        return cell
+    }
 }
