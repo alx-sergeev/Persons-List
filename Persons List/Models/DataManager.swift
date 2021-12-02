ad9bb393a967b36a7ad94a5f09b3d01bb671ccf5
Alexander Sergeev <sergeev_ul73@mail.ru>
2021-12-02 17:42:18 +0400

    Model: создал новую модель для хранения данных - DataManager

---
+++ Persons List/Models/DataManager.swift
@@ -0,0 +1,60 @@
+//
+//  DataManager.swift
+//  Persons List
+//
+//  Created by Сергеев Александр on 02.12.2021.
+//
+
+class DataManager {
+    static let arName = [
+        "Carl",
+        "Bruce",
+        "David",
+        "Edward",
+        "Alexander",
+        "Gloria",
+        "Isabella",
+        "Jessica",
+        "Katherine",
+        "Maria",
+    ]
+    
+    static let arLastName = [
+        "Douglas",
+        "Dyson",
+        "Ford",
+        "Fulton",
+        "Jacobson",
+        "Larkins",
+        "Mason",
+        "Osborne",
+        "Porter",
+        "Sherlock",
+    ]
+    
+    static let arEmail = [
+        "myemail_1@mail.ru",
+        "myemail_2@mail.ru",
+        "myemail_3@mail.ru",
+        "myemail_4@mail.ru",
+        "myemail_5@mail.ru",
+        "myemail_6@mail.ru",
+        "myemail_7@mail.ru",
+        "myemail_8@mail.ru",
+        "myemail_9@mail.ru",
+        "myemail_10@mail.ru"
+    ]
+    
+    static let arPhones = [
+        "453199527",
+        "837166287",
+        "958257578",
+        "867265274",
+        "525213714",
+        "964897948",
+        "415335230",
+        "017145894",
+        "001207048",
+        "106985760",
+    ]
+}
