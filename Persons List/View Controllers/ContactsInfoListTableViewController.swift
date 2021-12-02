4e9816503e125cd2c1f7a5832a5995dc69620210
Alexander Sergeev <sergeev_ul73@mail.ru>
2021-12-02 20:30:28 +0400

    Project: добавил новую вкладку Contacts Info, подготовительные работы

---
+++ Persons List/View Controllers/ContactsInfoListTableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ContactsInfoListTableViewController.swift
+//  Persons List
+//
+//  Created by Сергеев Александр on 02.12.2021.
+//
+
+import UIKit
+
+class ContactsInfoListTableViewController: UITableViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet weak var contactsTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
