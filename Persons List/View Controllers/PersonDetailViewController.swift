2adcaa15f571b3239c33b8b3ea3032559715f5a7
Alexander Sergeev <sergeev_ul73@mail.ru>
2021-12-02 18:41:00 +0400

    PersonDetailVC: готов верстка UI элементов, переход с главного экрана на этот

---
+++ Persons List/View Controllers/PersonDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PersonDetailViewController.swift
+//  Persons List
+//
+//  Created by Сергеев Александр on 02.12.2021.
+//
+
+import UIKit
+
+class PersonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
